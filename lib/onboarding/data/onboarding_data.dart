f365ddadbe4cf0c57687364452748e611c313f64
Iftekhar Ahmed <iftekharahmed728372@gmail.com>
2025-12-06 10:12:55 +0500

    Initial commit

---
+++ lib/onboarding/data/onboarding_data.dart
@@ -0,0 +1,8 @@
+import 'package:booksapp/onboarding/models/onboarding_model.dart';
+
+final List<OnboardingModel> onboardData =[
+  OnboardingModel(description: " Discover new worlds, join a vibrant reading community. Start your reading adventure effortlessly with us.", img: "assets/svgs/onboarding_image1.svg", title: "Now reading books will be easier"),
+  OnboardingModel(description: "Let us be your guide to the perfect read. Discover books tailored to your tastes for a truly rewarding experience.", img: "assets/svgs/onboarding_image2.svg", title: "Your Bookish Soulmate Awaits"),
+  OnboardingModel(description: "Ready to embark on a quest for inspiration and knowledge? Your adventure begins now. Let's go!", img: "assets/svgs/onboarding_image3.svg", title: "Start Your Adventure")
+
+];
---
+++ lib/onboarding/models/onboarding_model.dart
@@ -0,0 +1,6 @@
+class OnboardingModel{
+  final String img;
+  final String title;
+  final String description;
+  OnboardingModel({required this.description,required this.img,required this.title});
+}
